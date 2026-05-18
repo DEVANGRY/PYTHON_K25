811020ac59dd54a2d903849f1be59a82c4b275b2
QTuan <tuanlqhe173471@fpt.edu.vn>
2026-05-18 12:56:56 +0700

    first commit

---
+++ CNTT2_KS25/Session01/lythuyet.py
@@ -0,0 +1,22 @@
+import random
+
+# Khai báo biến
+    # Số nguyên
+    # Số thực
+    # Chuỗi
+    # Kiểu dữ liệu boolean (True/False)
+
+number_01 = 10
+number_02 = 20.5
+string_01 = "Hello, World!"
+boolean_01 = True
+boolean_02 = False
+
+# Hiển thị ra màn hình console log 
+print(f"Tôi năm nay {number_01} tuổi")
+
+# Lấy giá trị kiểu str từ người dùng nhập vào
+age_user = input("Mời bạn nhập tuổi của bạn")
+
+# in ra số random từ 100 -> 999
+print(random.randint(100,999))
